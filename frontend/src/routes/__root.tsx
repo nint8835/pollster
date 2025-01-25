cbbb7699491c3e6d274a9686a926188e4947f3dd
Riley Flynn <riley@rileyflynn.me>
2025-01-25 15:31:37 -0330

    Fix dev tools showing in prod

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,8 +1,8 @@
+import { TanStackRouterDevtools } from '@/components/util/router-dev-tools';
 import { queryClient } from '@/queries/client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
     component: () => (
