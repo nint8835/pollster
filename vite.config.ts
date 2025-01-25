1ffa66e8d1a1a03848c47c31e010b2f531558b7f
Riley Flynn <riley@rileyflynn.me>
2025-01-25 15:52:25 -0330

    Use vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -6,7 +7,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
     root: 'frontend',
-    plugins: [TanStackRouterVite(), react()],
+    plugins: [TanStackRouterVite(), react(), tailwindcss()],
     server: {
         proxy: {
             '/auth': {
