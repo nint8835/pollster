7d77fbb304cb6b9bb0e66314695e8f81a3435963
Riley Flynn <riley@rileyflynn.me>
2025-01-26 01:46:51 -0330

    Loading spinner

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { Spinner } from '@heroui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { StrictMode } from 'react';
@@ -6,10 +7,19 @@ import ReactDOM from 'react-dom/client';
 import { queryClient } from './queries/client';
 import { routeTree } from './routeTree.gen';
 
+function LoadingSpinner() {
+    return (
+        <div className="flex h-full flex-grow items-center justify-center">
+            <Spinner size="lg" />
+        </div>
+    );
+}
+
 const router = createRouter({
     routeTree,
     defaultPreload: 'intent',
     defaultPreloadStaleTime: 0,
+    defaultPendingComponent: LoadingSpinner,
 });
 
 declare module '@tanstack/react-router' {
